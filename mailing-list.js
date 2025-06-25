3025aa473100a36a8af70bde93c04020a93dba3b
Vihaan <vihaan.s.anand@gmail.com>
2025-06-25 16:56:07 -0500

    Add files via upload

--- mailing-list.js
+++ mailing-list.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", async function(event) {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData);
         try {
-                const response = await fetch("https://script.google.com/macros/s/AKfycbx4-RGoepx8ApyHVYFU0qlzUaDvBb4MeTer3e9wk-SXhG0t10_OQscpa2GYZ2XBiNtb/exec", {
+                const response = await fetch(atob("aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4NC1SR29lcHg4QXB5SFZZRlUwcWx6VWFEdkJiNE1lVGVyM2U5d2stU1hoRzB0MTBfT1FzY3BhMkdZWjJYQmlOdGIvZXhlYw=="), {
                         method: "POST",
                         body: new URLSearchParams(data)
                 });
